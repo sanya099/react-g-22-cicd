0d638515cf193a6a7f42ad859a74b2e2fca3265e
sanya099 <sanya099.com>
2025-05-01 20:33:04 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>My React App</h1>
+      <h2>This is a simple React app created by Sanya Bajaj </h2>
+      <h3>With github Action</h3>
+    </div>
+  )
+}
+
+export default App
